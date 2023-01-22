36e7fe34b21c4e452c2656fd9921e1822f482c0a
Nathan louissaint <louissaintmark@gmail.com>
2023-01-22 17:37:04 -0500

    nathan

--- nathan.js
+++ nathan.js
@@ -1,3 +1,5 @@
 function myFunction(){
     document.getElementById("demo").innerHTML=alert("nigga why you touching me")
+
 }
+
---
+++ strings.js
@@ -0,0 +1,9 @@
+//lets play around with some strings hehehhehe
+
+let nathan = "Google LLC is an American multinational technology company focusing on search engine technology, online advertising, cloud computing, computer software, quantum computing, e-commerce, artificial intelligence, and consumer electronics"
+.split("")
+.reverse("")
+.join("")
+console.log(nathan)
+
+
