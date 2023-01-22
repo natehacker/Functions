a8d14041edee88d08f0818c0ba49fc2ad32fc451
Nathan louissaint <louissaintmark@gmail.com>
2023-01-22 17:31:02 -0500

    more functions plus i added a palindrine

--- js.js
+++ js.js
@@ -10,3 +10,33 @@ return false
 return true 
 }
 console.log(isPalindrome(str))
+
+let strs = "bad"
+
+const isPalindromes =(inStrs)=>{
+    for (i=0;i< inStrs.length; i+=1){
+        if(inStrs[i] !==inStrs[inStrs.length -1 -i]){
+            return false
+
+        }
+
+
+    }
+    return true
+}
+console.log(isPalindromes(strs))
+
+let numbers = [3,45,6,6]
+number = numbers.map(double)
+function double (value){
+    return value * 3
+
+}
+console.log(number)
+
+for(i=0;i<1;i++){
+    console.log("rachel")
+
+}
+
+
