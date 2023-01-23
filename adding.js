489c01329ddfe1d7d0f9fc29a9f804f6649cfa99
Nathan louissaint <louissaintmark@gmail.com>
2023-01-23 08:28:33 -0500

    nathan

---
+++ adding.js
@@ -0,0 +1,16 @@
+// in this adding js file i will be practing adding functions
+
+function Add(num1,num2){
+console.log(num1 + num2)
+}
+Add(10,20)
+// how does adding functions work you may ask every function in javascript is only exicuted once it is called upon
+function one (num1,num2){
+    console.log(num1 + num2)
+}
+one(34,45)
+// this function will return the sum of 100
+function hello (num1,num3){
+    console.log(num1 + num3)
+}
+hello(50,50)
---
+++ unordered fucntions/js.js
@@ -0,0 +1,64 @@
+//palindrine checker 
+
+const str = "anoa"
+const isPalindrome = (inStr) => {
+    for(i=0; i<inStr.length; i+=1){
+    if(inStr[i] !== inStr[inStr.length -1 -i ]){
+return false
+    }
+}
+return true 
+}
+console.log(isPalindrome(str))
+
+let strs = "bad"
+
+// const isPalindromes =(inStrs)=>{
+//     for (i=0;i< inStrs.length; i+=1){
+//         if(inStrs[i] !==inStrs[inStrs.length -1 -i]){
+//             return false
+
+//         }
+
+
+//     }
+//     return true
+// }
+// console.log(isPalindromes(strs))
+
+
+
+let string = "mom"
+const isPalindromes = (string)=>{
+for(i=0;i<string.length; i+=1){
+if(string[i] !==string[string.length -1 -i]){
+    return false
+
+}
+}
+return true
+}
+console.log(isPalindromes)
+
+let numbers = [3,45,6,6]
+number = numbers.map(double)
+function double (value){
+    return value * 3
+
+}
+console.log(number)
+
+for(i=0;i<1;i++){
+    console.log("rachel")
+
+}
+
+
+// more map functions
+
+let people = [3,45,6,7,]
+let human = people.map(double)
+function double (value){
+    return value * 5
+}
+console.log(human)
