2c3d21cbdc531393302179d0e7f17c72fcc6e48b
Nathan louissaint <louissaintmark@gmail.com>
2023-01-31 15:26:28 -0500

    adding methods

---
+++ orderded functions/tuesday.js
@@ -0,0 +1,40 @@
+// im realizing that i need to become more organized with my work 
+
+//reverse a string()
+let string = "string"
+.split("")
+.reverse("")
+.join("")
+console.log(string)
+
+// reverse() method
+
+let array = [1,2,3,4]
+array.reverse()
+console.log(array)
+
+//push() method
+let arr = [1,2,3,4]
+arr.push(5,6)
+console.log(arr)
+
+// pop() method
+let newarr = [1,2,3,4,5]
+newarr.pop()
+newarr.pop()
+console.log(newarr)
+
+// use the sort() method 
+let numbers = [4,3,2,1]
+numbers.sort()
+console.log(numbers)
+
+
+//concat() method 
+
+const arr1 = [1,2]
+const arr2 = [3,4]
+const arr3 = [5,6]
+
+const allArr = arr1.concat(arr2,arr3)
+console.log(allArr)
